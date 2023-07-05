312f52c924da6e3b4ee56b687557a29ffae686d6
Feroz Khan <50706350+Fer0zkhan@users.noreply.github.com>
2023-07-05 14:36:46 +0500

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use('/driver', driverRoute);
 
 //DataBase Connectivity
 mongoose.connect(process.env.DATABASE_NAME, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
-    console.log("Connection Ready");
+    console.log("Database connection Ready");
 });
 //LocalHost Server
-app.listen(process.env.PORT || 3000, () => console.log('Server running!!'));
+app.listen(process.env.PORT || 3000, () => console.log(`Server running at port ${process.env.PORT || 3000}`));
