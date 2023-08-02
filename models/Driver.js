a02036889d150ef7d1b80cfc52ebc60188548db0
Feroz Khan <50706350+Fer0zkhan@users.noreply.github.com>
2023-08-02 17:09:18 +0500

    Update Driver.js

--- models/Driver.js
+++ models/Driver.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const driverSchema = mongoose.Schema({
+const driverSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
