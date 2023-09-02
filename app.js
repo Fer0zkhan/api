8019f6c96cd60b283058a2858ede658fcd1d3169
Feroz Khan <50706350+Fer0zkhan@users.noreply.github.com>
2023-09-02 13:54:14 +0500

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const expressSession = require('express-session');
 require('dotenv/config');
 
-//Import routes
+//Import Routes
 const adminRoute = require('./routes/admin');
 const customerRoute = require('./routes/customer');
 const driverRoute = require('./routes/driver');
