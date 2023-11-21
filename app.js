2a93635c0129cbca745b293785506285b85a8530
Feroz Khan <50706350+Fer0zkhan@users.noreply.github.com>
2023-11-21 16:27:59 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+require('dotenv/config');
+
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const expressSession = require('express-session');
-require('dotenv/config');
 
 //Import Routes
 const adminRoute = require('./routes/admin');
