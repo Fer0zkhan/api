61e7e0a806ce85d9c287c063936ec6061ca63ced
Feroz Khan <50706350+Fer0zkhan@users.noreply.github.com>
2024-07-11 18:24:06 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
+require('dotenv/config');
+
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const expressSession = require('express-session');
+
+//Import Routes
+const adminRoute = require('./routes/admin');
+const customerRoute = require('./routes/customer');
+const driverRoute = require('./routes/driver');
+
+//middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use('/admin', adminRoute);
+app.use('/customer', customerRoute);
+app.use('/driver', driverRoute);
+
+//Database connectivity
+mongoose.connect(process.env.DATABASE_NAME, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+    console.log("Database connection successfully");
+});
+
+//Server
+app.listen(process.env.PORT || 3000, () => console.log(`Server running at port ${process.env.PORT || 3000}`))
+
+
+
+
 
